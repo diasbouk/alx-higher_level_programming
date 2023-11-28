482dabcd0ef08d0c3006de79a063e8784894279a
diasbouk <boukdsaid@gmail.com>
2023-11-28 13:07:55 +0100

    TASK 11

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
-print('and that piece of art is useful - Dora Korpar, 2015-10-19' , file=sys.stderr)
+file=sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19')
 exit(1)
