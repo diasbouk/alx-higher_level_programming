91d8733f1c38c44fb9802af2f9af85faa97e376c
diasbouk <boukdsaid@gmail.com>
2023-11-29 14:40:01 +0100

    TASK 11

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
-string = "that piece of art is useful - Dora Korpar, "
+string = "and that piece of art is useful - Dora Korpar, "
 file=sys.stderr.write("{}{}-{}-{}".format(string, 2015, 10, 19))
 exit(1)
