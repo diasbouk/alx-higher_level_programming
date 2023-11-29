3778df712283e89cdca46bc118d1fa4c9c4e63c2
diasbouk <boukdsaid@gmail.com>
2023-11-29 02:05:15 +0100

    TASK 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-new_str = ""
+
 def remove_char_at(str, n):
-    for i in range(0, len(str)):
-        if (i != n):
-            new_str[i] = str[i]
-    return new_str
+    return str[0: n] + str[n + 1: len(str)]
+
+
+print(remove_char_at("Best School", 3))
+print(remove_char_at("Chicago", 2))
+print(remove_char_at("C is fun!", 0))
+print(remove_char_at("School", 10))
+print(remove_char_at("Python", -2))
