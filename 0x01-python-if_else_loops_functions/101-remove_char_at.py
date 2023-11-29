f375ac92450931fe716cc0cd313e8882104551c9
diasbouk <boukdsaid@gmail.com>
2023-11-29 02:05:50 +0100

    TASK 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
 def remove_char_at(str, n):
     return str[0: n] + str[n + 1: len(str)]
-
-
-print(remove_char_at("Best School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
