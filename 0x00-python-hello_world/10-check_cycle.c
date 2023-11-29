d97111684443bc563e75d91f4646c52a83f4c610
diasbouk <boukdsaid@gmail.com>
2023-11-29 14:13:04 +0100

    TASK 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,7 +12,7 @@ int check_cycle(listint_t *list)
     listint_t *slow = list;
     listint_t *fast = list;
 
-    if (list == NULL)
+    if (list == NULL || list->next == NULL)
         return (0);
         
     while (fast)
