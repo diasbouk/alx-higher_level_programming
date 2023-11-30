d91316f079bf2cc049a8002732326f0b14b9445d
diasbouk <boukdsaid@gmail.com>
2023-11-30 22:27:36 +0100

    TASK 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if argv[2] == '+':
+    elif argv[2] == '+':
         result = add(int(argv[1]), int(argv[3]))
         print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
 
