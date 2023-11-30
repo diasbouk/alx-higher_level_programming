e5a222effa91848b87419e1df14b083c002900f6
diasbouk <boukdsaid@gmail.com>
2023-11-30 21:56:41 +0100

    TASK 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,13 +5,22 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
     if argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/':
         print("Unknown operator. Available operators: +, -, * and /")
+
     if argv[2] == '+':
-        print("{} {} {} = {}".format(argv[1], argv[2], argv[3], add(int(argv[1]), int(argv[3]))))
+        result = add(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
+
     elif argv[2] == '-':
-        print("{} {} {} = {}".format((argv[1], argv[2], argv[3], sub(int(argv[1]), int(argv[3])))))
+        result = sub(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {}".format((argv[1], argv[2], argv[3], result)))
+
     elif argv[2] == '*':
-        print("{} {} {} = {}".format(argv[1], argv[2], argv[3], mul(int(argv[1]), int(argv[3]))))
+        result = mul(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
+
     elif argv[2] == '/':
-        print("{} {} {} = {}".format(argv[1], argv[2], argv[3], div(int(argv[1]), int(argv[3]))))
+        result = div(int(argv[1]), int(argv[3]))
+        print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
