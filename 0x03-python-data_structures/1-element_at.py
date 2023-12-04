5fa53396164a27db6dff52535cbadd62a63c91fe
diasbouk <boukdsaid@gmail.com>
2023-12-04 14:10:10 +0100

    TASK 0

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     if idx < 0:
         return None
     elif idx > len(my_list) - 1:
-        retrun None
+        return None
     else:
         return my_list[idx]
