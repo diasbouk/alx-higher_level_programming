c917e9557e5338151265c22c3971cee6af9512a2
diasbouk <boukdsaid@gmail.com>
2023-12-04 14:30:15 +0100

    TASK 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     if idx >= 0 and idx < len(my_list):
         new_list = my_list
         new_list[idx] = element
