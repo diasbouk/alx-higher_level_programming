ee88311794f9ff278ea0a1e2ffdac601cea68855
diasbouk <boukdsaid@gmail.com>
2023-12-04 21:45:49 +0100

    TASK 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        return 0, None
-    else:
-        return len(sentence), sentence[0]
+        sentence[0] = None
+    return len(sentence), sentence[0]
