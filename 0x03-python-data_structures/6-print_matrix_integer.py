0cb2e702f21b27552d9d8eb84c11946e8a525cbb
diasbouk <boukdsaid@gmail.com>
2023-12-04 15:03:52 +0100

    TASK 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for element in matrix:
         for number in element:
-            print("{:d} ".format(number),end="")
+            print("{:d} ".format(number), end="")
         print("")
