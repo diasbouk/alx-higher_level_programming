2e06b6a6a5659bbb72a3870205613d9f2aa0341c
diasbouk <boukdsaid@gmail.com>
2023-12-06 13:52:07 +0100

    TASK 00

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 def square_matrix_simple(matrix=[]):
     i = 0
     j = 0
-    new_matrix = matrix
+    new_matrix = [[]]
     for i in  range(0, len(matrix)):
         for j in range(0, len(matrix[i])):
             new_matrix[i][j] = matrix[i][j] * matrix[i][j]
