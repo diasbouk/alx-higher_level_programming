b153f48c446a00825a1c24333620d5026bbd8c83
diasbouk <boukdsaid@gmail.com>
2023-12-06 14:20:00 +0100

    TASK 00

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+import copy
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix
+    new_matrix = copy.deepcopy(matrix)
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix[i])):
             new_matrix[i][j] = matrix[i][j] * matrix[i][j]
