a7891696af464229cb9d3c6118bd14e4ca660ecb
diasbouk <boukdsaid@gmail.com>
2023-12-08 21:14:32 +0100

    Task 13 : Added condit in Code

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,8 +2,10 @@
 def weight_average(my_list=[]):
     scores = 0
     sums = 0
-    dict(my_list)
-    for elem in my_list:
-        sums += elem[1]
-        scores += elem[0] * elem[1]
-    return scores / sums
+    if len(my_list) > 0:
+        dict(my_list)
+        for elem in my_list:
+            sums += elem[1]
+            scores += elem[0] * elem[1]
+        return scores / sums
+    return 0
