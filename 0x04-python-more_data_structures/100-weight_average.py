0586fbfc2c567b5888586887d374b3acdae13383
diasbouk <boukdsaid@gmail.com>
2023-12-08 21:12:07 +0100

    Task 100 : Code check

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pytohn3
+def weight_average(my_list=[]):
+    scores = 0
+    sums = 0
+    dict(my_list)
+    for elem in my_list:
+        sums += elem[1]
+        scores += elem[0] * elem[1]
+    return scores / sums
