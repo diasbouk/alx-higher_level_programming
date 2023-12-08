436c4a5d44a8257bd0e57eb745d79dd5d5ed9315
diasbouk <boukdsaid@gmail.com>
2023-12-08 15:55:24 +0100

    Task 9 : Code checked

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = {}
-    for elem in a_dictionary:
-        number = map(lambda num: num * 2, elem)
-        new_dict.append(list(number))
+    new_dict = a_dictionary.copy()
+    for key in new_dict.keys():
+        new_dict[key] *= 2
     return new_dict
