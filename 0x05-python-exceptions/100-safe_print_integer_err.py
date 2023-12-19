d7dc4626ca956adb81564b1984c5b7e8bedd9a9c
diasbouk <boukdsaid@gmail.com>
2023-12-19 08:37:01 +0000

    Task 7 : error rasie handle

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 from sys import stderr
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except ValueError as val:
+        print(val, file=stderr)
         return False
