f9b0a080e1d61ab6f212a5bdbb577995654af590
diasbouk <boukdsaid@gmail.com>
2023-12-19 08:50:22 +0000

    Task 8 : Syntax error , all done

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         return fct(*args)
     except ZeroDivisionError as err:
-        print("Excepiton: {}".format(err), file=stderr)
+        print("Exception: {}".format(err), file=stderr)
     except IndexError as err:
         print("Exception: {}".format(err), file=stderr)
