e8f8239fcef0e19d3347b75fc73c5b943b58b4a0
diasbouk <boukdsaid@gmail.com>
2023-12-20 09:33:52 +0100

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""A square class based on 0"""
+"""Define a class Square"""
 
 
 class Square:
+    """representing square operation"""
     def __init__(self, size):
-        """Initializing Square
-        Args:
-        size 
-    """
-        self.__size = size
 
+        """intialize the square.
+
+        Args:
+            size : size of the square
+        """
+        self.__size = size
