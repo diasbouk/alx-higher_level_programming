a54fa43b6c2ab9b52bd73100b06940c370de6f02
diasbouk <boukdsaid@gmail.com>
2023-12-20 12:43:43 +0100

    Task 2 : PyCodeStyle Done

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,10 +7,8 @@ class Square:
     def __init__(self, size=0):
         """assgin size"""
         self.__size = size
-
-
         """checking size type and value"""
-        if type(size) is  not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
