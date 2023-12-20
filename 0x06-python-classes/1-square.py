375053ec45d6c20b1a8b9f8c0baa46b6577d8d36
diasbouk <boukdsaid@gmail.com>
2023-12-20 09:32:48 +0100

    Task 1 : Code check

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A square class based on 0"""
+
+
+class Square:
+    def __init__(self, size):
+        """Initializing Square
+        Args:
+        size 
+    """
+        self.__size = size
+
