f33c8970d98b0ef32e9adf1324c72f527ce7abbd
diasbouk <boukdsaid@gmail.com>
2023-12-20 21:05:00 +0100

    Task 6 Checking again

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,50 +1,53 @@
 #!/usr/bin/python3
-""" A class with update """
-
-
 class Square:
-    """Square stuff"""
+    """Defines a square"""
     def __init__(self, size=0, position=(0, 0)):
-        """assgin size"""
-        self.__size = size
-        """checking size type and value"""
-    """Function to count area"""
+        """Initialises the data"""
+        self.size = size
+        self.position = position
+
     def area(self):
-        return self.__size * self.__size
+        """Returns current square area"""
+        return self.__size**2
 
     @property
     def size(self):
-        """The size property."""
+        """Getter method"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the value"""
-        if type(value) is not int:
+        """Setter method"""
+        self.__size = value
+        if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+
+    def my_print(self):
+        """Prints the square"""
+        if self.__size == 0:
+            print()
         else:
-            self.__size = value
+            for y in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for x in range(self.__position[0]):
+                    print(' ', end='')
+                for j in range(self.__size):
+                    print('#', end='')
+                print()
 
     @property
     def position(self):
-        """The position property."""
+        """Getter method"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Set the value"""
-        if type(value) is not tuple:
-            raise TypeError("position must be a tuple of 2 positive ")
-        else:
-            self.__position = value
-
-    def my_print(self):
-        if self.__size == 0:
-            print()
-        else:
-            for i in range(0, self.__size):
-                for j in range(0, self.__size):
-                    print("#", end="")
-                print()
+        """Setter method"""
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if any(type(i) != int for i in value) or any(j < 0 for j in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
