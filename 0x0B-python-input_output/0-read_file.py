03bf2cde8d79c94851eb6601a4072f4e28e233a4
diasbouk <boukdsaid@gmail.com>
2024-01-09 22:40:15 +0100

    TASK 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@ Reads from a file and prints
 to stdout"""
 
 def read_file(filename=""):
-    with open(filename, "r", encoding='utf-8') as file:
+    with open(filename, "r", encoding='UTF-8') as file:
         content = file.read()
         print("{}".format(content))
