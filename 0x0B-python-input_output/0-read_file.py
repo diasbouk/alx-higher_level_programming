1b83a365d917fa561ab17a4fb9e02978ab5e31a3
diasbouk <boukdsaid@gmail.com>
2024-01-09 22:53:00 +0100

    TASK 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""
-Reads from a file and prints
-to stdout"""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding='UTF-8') as file:
-        print("{}".format(file.read()), end='')
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
