c3bf6405f376c2a818b4762a65036dd2b9d2dfa7
diasbouk <boukdsaid@gmail.com>
2024-03-20 02:36:08 +0000

    task 14

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Python.h>
+
+void print_python_list_info(PyObject *p)
+{
+	int i;
+		printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+		printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+		for (i = 0; i < Py_SIZE(p); i++)
+			printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+}
