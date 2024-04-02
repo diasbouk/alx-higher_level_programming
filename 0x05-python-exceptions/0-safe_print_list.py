e969a47fec99f6c8b55c4fe70814e98d0bd5da26
diasbouk <boukdsaid@gmail.com>
2024-04-02 21:03:32 +0000

    $commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,6 +3,6 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for ele in range(1, x):
-            print(my_list[ele - 1], end="")
+            print("{}".format(my_list[ele - 1]), end="")
     except IndexError:
         print("Out of range")
