80afcc41f244c4af544cbb962e9b04849cabc6d7
diasbouk <boukdsaid@gmail.com>
2024-04-02 21:02:44 +0000

    $commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 
-
+def safe_print_list(my_list=[], x=0):
+    try:
+        for ele in range(1, x):
+            print(my_list[ele - 1], end="")
+    except IndexError:
+        print("Out of range")
