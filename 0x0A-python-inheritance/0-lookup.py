c85c38d1940c6bc90b55ea8a3b6b47594b39c424
diasbouk <boukdsaid@gmail.com>
2024-04-15 11:56:00 +0100

    task 0 done ==>

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,8 +4,8 @@ def lookup(obj):
     """
     Looks for an object infos
     Args:
-    obj (object): Object to look at 
-    Returns: Info about abj attrs 
+    obj (object): Object to look at
+    Returns: Info about abj attrs
 
     """
     return dir(obj)
