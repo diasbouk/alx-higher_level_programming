f84bf9893af1e1ae2e5fadf035ab37b4951a7d2d
diasbouk <boukdsaid@gmail.com>
2024-04-17 15:31:47 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ For modules and other stuff
 
 def read_file(filename=""):
     """ Reads from a file and prints to stdout """
-    with open(filename, "r+", encoding="utf-8") as f:
-        print(line for line in f)
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
