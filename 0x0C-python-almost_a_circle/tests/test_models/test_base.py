383bc498e39f1ac9366ee60bea91051f013913d4
diasbouk <boukdsaid@gmail.com>
2024-04-23 13:35:23 +0100

    tests

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+""" MODULES AND IMPORT """
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestBase(unittest.TestCase):
+    """ Inherites from Testcase """
+
