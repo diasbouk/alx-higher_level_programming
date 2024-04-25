748632bce14f260f5a98c94b38cf9c686a86c789
diasbouk <boukdsaid@gmail.com>
2024-04-25 19:53:54 +0100

    task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,8 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor"""
         super().__init__(size, size, x, y, id)
+        self.width = size
+        self.height = size
 
     def __str__(self):
         """String repesenting of the class"""
