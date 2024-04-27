688e6b7a8638a282a5a60deb81aea66c964c75c0
diasbouk <boukdsaid@gmail.com>
2024-04-27 17:07:10 +0100

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" For import and stuff """
+
+def append_write(filename="", text=""):
+    """ Apeends text to filename """
+    with open(filename, "a+", encoding="utf-8") as file:
+        return file.write(text)
