707930e81dfacdefe49c0dd603035b7f2002f61e
diasbouk <boukdsaid@gmail.com>
2024-04-27 17:12:38 +0100

    task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -6,4 +6,4 @@ import json
 
 def from_json_string(my_str):
     """ returns an object (Python data structure) represented by a JSON string """
-    return json.load(my_str)
+    return json.loads(my_str)
