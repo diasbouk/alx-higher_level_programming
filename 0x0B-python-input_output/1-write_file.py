6a753d97c66e20dadab40744dcefe046ea6981ab
diasbouk <boukdsaid@gmail.com>
2024-04-27 17:02:31 +0100

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,8 +2,8 @@
 
 """ Docs """
 
+
 def write_file(filename="", text=""):
     """ writes to file """
     with open(filename, 'w', encoding='UTF-8') as f:
-        return  f.write(text)
-
+        return f.write(text)
