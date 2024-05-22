7e69da932fb056a16e7bce445207dd90805f180b
diasbouk <boukdsaid@gmail.com>
2024-05-22 22:32:24 +0100

    task  9 - Create second_table and insert some records

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Creates a table second_table in the database hbtn_0c_0
-CREATE TABLE IF NOT EXIST second_table (
+CREATE TABLE IF NOT EXISTS second_table (
 	id int,
 	name varchar(256),
 	score int
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXIST second_table (
 
 -- Creates new records
 
-INSERT INTO second_table (`id`, `name`, `score`) VALUES (1, "John", 10);
-INSERT INTO second_table (`id`, `name`, `score`) VALUES (2, "Alex", 3);
-INSERT INTO second_table (`id`, `name`, `score`) VALUES (3, "Bob", 14);
-INSERT INTO second_table (`id`, `name`, `score`) VALUES (4, "George", 8);
+INSERT INTO second_table  VALUES (1, "John", 10);
+INSERT INTO second_table  VALUES (2, "Alex", 3);
+INSERT INTO second_table  VALUES (3, "Bob", 14);
+INSERT INTO second_table  VALUES (4, "George", 8);
