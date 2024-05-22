16e3d95e4b934dee113e65355c793d1a5d402b8c
diasbouk <boukdsaid@gmail.com>
2024-05-22 12:52:32 +0100

    create second_table and insert new records

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Creates a table second_table in the database hbtn_0c_0
+CREATE TABLE IF NOT EXIST second_table (
+	id int,
+	name varchar(256)
+	score int
+);
+
+-- Creates new records
+
+INSERT INTO second_table VALUES(1, 'Jhon', 10);
+INSERT INTO second_table VALUES(2, 'Jhon', 3);
+INSERT INTO second_table VALUES(3, 'Jhon', 14);
+INSERT INTO second_table VALUES(4, 'Jhon', 8);
