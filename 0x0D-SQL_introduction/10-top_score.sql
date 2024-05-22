d4109f4c17125c7ce2843f4b6d3de2a89cd3ec69
diasbouk <boukdsaid@gmail.com>
2024-05-22 22:39:40 +0100

    task 10 select oredered by score

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -3,4 +3,4 @@
     -- Results should display both the score and the name (in this order)
     -- Records should be ordered by score (top first)
     -- The database name will be passed as an argument of the mysql command
-SELECT score, name FROM second_table ORDER BY score;
+SELECT score, name FROM second_table ORDER BY score DESC;
