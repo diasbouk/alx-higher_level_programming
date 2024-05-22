586da69d62899d65969317396d3dca95eb958343
diasbouk <boukdsaid@gmail.com>
2024-05-22 22:45:03 +0100

    task 11 - fixed

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -4,5 +4,5 @@
 --     Records should be ordered by score (top first)
 --	   The database name will be passed as an argument of the mysql command
 
-SELECT * FROM second_table WHERE score>=10 ORDER BY score DESC;
+SELECT * FROM second_table WHERE score >= 10 ORDER BY score DESC;
 
