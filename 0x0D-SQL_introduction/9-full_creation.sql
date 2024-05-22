503242ad907aa53eb9b9270231c53f15bbc7f760
diasbouk <boukdsaid@gmail.com>
2024-05-22 22:29:30 +0100

    task 9 - insertion after creating a second_table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXIST second_table (
 
 -- Creates new records
 
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (4, "George", 8);
