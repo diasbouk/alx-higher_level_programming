e76122f7744bad46b70169d33ecdf3ebb7fb34f5
diasbouk <boukdsaid@gmail.com>
2024-05-22 22:36:33 +0100

    task 10 - select name and record | Ordered by score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- Lists all records of the table second_table of the database hbtn_0c_0
+
+    -- Results should display both the score and the name (in this order)
+    -- Records should be ordered by score (top first)
+    -- The database name will be passed as an argument of the mysql command
+SELECT score, name FROM second_table OREDER BY score
