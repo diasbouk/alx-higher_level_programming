285de8a46abe58ce080187a10ca8dba66a2a2159
diasbouk <boukdsaid@gmail.com>
2024-05-23 02:08:26 +0100

    task 0 - show grants

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on our server (in localhost).
 
-SHOW GRANTS FOR *;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
