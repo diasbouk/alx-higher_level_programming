19f35ba20379ac903a41b2ddd9a4ab7d0325b834
diasbouk <diasbouk>
2024-07-28 21:49:34 +0100

    Task 10 done and goood

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const av = process.argv;
+function factorialRecursively (result, number) {
+  if (result === 1 || result === 0) {
+    return 1;
+  }
+  if (number === 1) {
+    return (result);
+  }
+  return factorialRecursively(result * number, number - 1);
+}
+if (isNaN(av[2])) {
+  console.log(1);
+} else {
+  console.log(factorialRecursively(Number(av[2]), Number(av[2]) - 1));
+}
