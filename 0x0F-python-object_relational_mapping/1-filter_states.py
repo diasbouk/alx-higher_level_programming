75ada253e6e7407e8e694cba296b99d8a3a11dc1
diasbouk <boukdsaid@gmail.com>
2024-08-03 16:43:43 +0100

    Task 1 done, all records with name n%

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Import modules: MySQLdb to interacat with our db
+sys.argv to use cli args"""
+
+import MySQLdb
+from sys import argv as av
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                         user=av[1],
+                         passwd=av[2],
+                         db=av[3],
+                         port=3306)
+    cur = db.cursor()
+    num_rows = cur.execute('SELECT * FROM states WHERE name LIKE "N%"')
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
